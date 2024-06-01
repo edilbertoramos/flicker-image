b77216dead89e9fefa6e073287916f9922225b51
Edilberto Ramos <edilberto.ramos@icloud.com>
2024-06-01 18:48:39 -0400

    Change app name

--- FlickerImage/FlickrImageApp.swift
+++ FlickerImage/FlickrImageApp.swift
@@ -1,5 +1,5 @@
 //
-//  FlickerImageApp.swift
+//  FlickrImageApp.swift
 //  FlickerImage
 //
 //  Created by Edilberto Ramos on 01/06/24.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct FlickerImageApp: App {
+struct FlickrImageApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
