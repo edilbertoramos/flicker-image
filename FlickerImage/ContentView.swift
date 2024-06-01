1fd6fe1ee7aeb8055a22f9897bcdb4dbc3edde99
Edilberto Ramos <edilberto.ramos@icloud.com>
2024-06-01 12:03:42 -0400

    Create Xcode project

---
+++ FlickerImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlickerImage
+//
+//  Created by Edilberto Ramos on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlickerImage/FlickerImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerImageApp.swift
+//  FlickerImage
+//
+//  Created by Edilberto Ramos on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickerImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
